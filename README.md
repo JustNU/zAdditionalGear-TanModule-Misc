f87fe259afa6ce9b5abbe3760fb21f325872fdc1
JustNU <ivancool108@gmail.com>
2022-12-22 03:47:21 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Additional Gear - Tan Module, Misc.
+Part of "Additional N" series of mods, requires my core. Tan retextures/recolors of misc equipment (masks, glasses, etc.).
---
+++ src/mod.js
@@ -0,0 +1,56 @@
+﻿"use strict";
+
+class Mod
+{
+	
+	postDBLoad(container) 
+	{
+		// Constants
+		const logger = container.resolve("WinstonLogger");
+		const database = container.resolve("DatabaseServer").getTables();
+		const jsonUtil = container.resolve("JsonUtil");
+		const core = container.resolve("JustNUCore");
+		const modDb = `user/mods/zAdditionalGear-TanModule/db/`;
+		const config = require("../config/config.json");
+		const itemConfig = require("../config/itemConfig.json");
+		const itemData = require("../db/items/itemData.json");
+		
+		//add retextures
+		for (const categoryId in itemConfig) {
+			for (const itemId in itemConfig[categoryId]) {
+				if (itemConfig[categoryId][itemId]) {
+					core.addItemRetexture(modDb, itemId, itemData[itemId].BaseItemID, itemData[itemId].BundlePath, config.EnableTradeOffers, config.AddToBots);
+				}
+			}
+		}
+		
+		// debug
+		const debug = true;
+		
+		if (debug) {
+			for (const item in database.templates.items) {
+				database.templates.items[item]._props.ExaminedByDefault = true;
+			}
+		}
+		
+		// Modify quests
+		if (config.EnableQuestChanges) {
+			const punisher4Gear = [
+				["572b7adb24597762ae139821", "AddGearTan_CFBalaclava"],
+				["572b7adb24597762ae139821", "AddGearTan_MomexBalaclava"]
+			];
+			
+			// The Punisher - Part 4
+			if (database.templates.quests["59ca264786f77445a80ed044"]) {
+				const thePunisher4Gear = database.templates.quests["59ca264786f77445a80ed044"].conditions.AvailableForFinish[1]._props.counter.conditions[2]._props.equipmentInclusive;
+				
+				database.templates.quests["59ca264786f77445a80ed044"].conditions.AvailableForFinish[1]._props.counter.conditions[2]._props.equipmentInclusive = [
+					...jsonUtil.clone(thePunisher4Gear),
+					...punisher5Gear
+				];
+			}
+		}
+	}
+}
+
+module.exports = { mod: new Mod() }
